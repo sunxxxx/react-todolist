a207494236f50a3bcbe2357a1bce303e1426cce2
sunxx <sunxx@amegroups.com>
2019-08-13 16:03:29 +0800

    save

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+// import { thisExpression } from '@babel/types';
 
 
 
@@ -8,8 +9,10 @@ class App extends React.Component {
     super(props);
     this.state = {
       list: [],
+      allList: [],
       checkList: [],
-      defaultChecked: false
+      activeList: [],
+      completedList: []
     };
     this.toggleChecked = this.toggleChecked.bind(this)
   }
@@ -20,46 +23,64 @@ class App extends React.Component {
 
   add(e){
     if(window.event.keyCode === 13 && e.target.value){
-      this.setState({list:[...this.state.list,e.target.value]})
+      var i = this.state.allList.length
+      let newItem = {
+        id: i,
+        value:e.target.value,
+        checked: false
+      }
+      this.setState({allList:[...this.state.allList,newItem]})
+      this.setState({list:[...this.state.allList,newItem]})
+      // let {list}= this.state
+      // list.push({value:e.target.value})
+      // this.setState({list})
         e.target.value = '' 
       }
   }
 
 
   changeAll(){
-
+    this.setState({list:this.state.allList})
   }
 
   changeActive(){
-    var newList = this.state.list.filter(function (todo) {
-      return !todo.completed;
+    this.state.activeList = this.state.allList.filter(function (todo) {
+      return !todo.checked;
     })
-
-    this.setState(this.state.list,newList)
+    this.setState({list:this.state.activeList})
   }
 
   changeCompleted(){
-    this.state.list = this.state.list.filter(function (todo) {
-			return todo.completed;
-		});
+    this.state.completedList = this.state.allList.filter(function (todo) {
+      return todo.checked;
+    });
+    this.setState({list:this.state.completedList})
   }
 
   toggleChecked(e,index){
-    console.log(index)
     this.state.list[index].checked = e.target.checked
-    this.setState({list:this.state.list},()=>{
-      console.log(this.state.list)
-    })
+    this.setState({list:this.state.list})
   }
 
-  cancle(e,index){
-    console.log(e,index)
+  delect(index){
+    let indexNumber = this.state.allList.indexOf(this.state.list[index])
+    this.state.list.splice(index,1)
+    this.state.allList.splice(indexNumber,1)
+    this.setState({list:this.state.list})
+    this.setState({allList:this.state.allList})
   }
 
-  allChecked(e){
-    // this.setState(state => {
-    //   return { counters: [...state.counters, obj] };
-    // });
+  allChecked(){
+    if(this.state.list[0].checked == false){
+      for(let i=0;i<this.state.list.length;i++){
+        this.state.list[i].checked = true
+      }
+    }else{
+      for(let i=0;i<this.state.list.length;i++){
+        this.state.list[i].checked = false
+      }
+    }
+    this.setState({list:this.state.list})
   }
 
   render(){
@@ -76,11 +97,12 @@ class App extends React.Component {
             <i className="allChecked" onClick={this.allChecked.bind(this)}>></i>
           </div>
           <div className="listBox">
-            {this.state.list.map((todo,index,key) => (
-              <li key={todo.id}>{todo}
-                <input type="checkbox" onChange={(e) => this.toggleChecked(e,index)} checked={this.state.defaultChecked} />
-                <span className="right cancle" onClick={this.cancle.bind(this,todo.id)}>+</span>
-              </li>
+            {this.state.list.map((todo,index) => (
+              <div key={'li'+todo.id}>
+                <input type="checkbox" onChange={(e) => this.toggleChecked(e,index)} checked={todo.checked} />
+                <label className={todo.checked == true ? 'lineThrough': ''}>{todo.value}</label>
+                <span className="right cancle" onClick={this.delect.bind(this,index)}>+</span>
+              </div>
 
             ))}
           </div>
